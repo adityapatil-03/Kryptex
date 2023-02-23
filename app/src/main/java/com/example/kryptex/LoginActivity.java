db354a140eed3320d80ab24e1efa206064edc8bf
aditya <adityapatil942328@gmail.com>
2023-02-23 16:53:24 +0530

    NA-4 Login Page added

--- app/src/main/java/com/example/kryptex/LoginActivity.java
+++ app/src/main/java/com/example/kryptex/LoginActivity.java
@@ -11,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
     EditText input ;
-    TextView ad;
+    TextView ad,BD;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
